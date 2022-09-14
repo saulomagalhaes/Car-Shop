78e875149b23b51af910791399b5ed1902e59bad
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-14 20:42:27 -0300

    feat(Routes): :sparkles: implementando a rota post pro Car

---
+++ src/routes/cars.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/Car';
+import CarModel from '../models/Car';
+import CarService from '../services/Car';
+
+const route = Router();
+
+const carModel = new CarModel();
+const carService = new CarService(carModel);
+const carController = new CarController(carService);
+
+route.post('/cars', (req, res) => carController.create(req, res));
+
+export default route;

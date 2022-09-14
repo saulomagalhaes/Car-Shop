2e30e46c240758d2f30e72047a19f45eb31f2823
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-14 20:39:55 -0300

    feat(app): :sparkles: inserindo o express async errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import carRouter from './routes/cars';
 
 const app = express();
+app.use(express.json());
+app.use(carRouter);
+
+app.use(errorHandler);
 
 export default app;

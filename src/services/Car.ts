bb5301e3de9e1568bd708ffb035b35a63ea5cc6c
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-14 20:38:40 -0300

    feat(CarService): :sparkles: implementando CarService

---
+++ src/services/Car.ts
@@ -0,0 +1,20 @@
+import { carZodSchema, ICar } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import { IService } from '../interfaces/IService';
+
+class CarService implements IService<ICar> {
+  private _car:IModel<ICar>;
+  constructor(model:IModel<ICar>) {
+    this._car = model;
+  }
+
+  public async create(obj:unknown):Promise<ICar> {
+    const parsed = carZodSchema.safeParse(obj);
+    if (!parsed.success) {
+      throw parsed.error; 
+    }
+    return this._car.create(parsed.data);
+  }
+}
+
+export default CarService;

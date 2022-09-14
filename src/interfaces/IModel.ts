ad850fd0e19da8e35c79d5b2ed8e94ba34568938
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-14 15:34:44 -0300

    refactor(IModel): :recycle: correção do retorno da promise do método readOne e mudança no nome dos parametros

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,7 +1,7 @@
 export interface IModel<T> {
   create(obj: T): Promise<T>;
   read(): Promise<T[]>;
-  readOne(string: string): Promise<T>;
-  update(string: string, obj: T): Promise<T | null>;
-  delete(string: string): Promise<T | null>;
+  readOne(_id: string): Promise<T | null>;
+  update(_id: string, obj: T): Promise<T | null>;
+  delete(_id: string): Promise<T | null>;
 }

7faac146c44814b3662f2cbe4a20ce048c969cd5
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-14 20:41:12 -0300

    refactor(Interfaces): :recycle: modificações em algumas interfaces

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,10 +1,24 @@
 import { z } from 'zod';
 import { vehicleZodSchema } from './IVehicle';
 
-const carZodSchema = vehicleZodSchema.extend({
-  doorsQty: z.number().int().positive().min(2)
+export const carZodSchema = vehicleZodSchema.extend({
+  doorsQty: z
+    .number({
+      required_error: 'doorsQty is required',
+      invalid_type_error: 'doorsQty must be a number',
+    })
+    .int()
+    .positive()
+    .min(2)
     .max(4),
-  seatsQty: z.number().int().min(2).max(7),
+  seatsQty: z
+    .number({
+      required_error: 'seatsQty is required',
+      invalid_type_error: 'seatsQty must be a number',
+    })
+    .int()
+    .min(2)
+    .max(7),
 });
 
 export type ICar = z.infer<typeof carZodSchema>;
--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,11 +1,33 @@
 import { z } from 'zod';
 
 export const vehicleZodSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().int().min(1900).max(2022),
-  color: z.string().min(3),
+  model: z
+    .string({
+      required_error: 'Model is required',
+      invalid_type_error: 'Color must be a string',
+    })
+    .min(3),
+  year: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be a number',
+    })
+    .int()
+    .min(1900)
+    .max(2022),
+  color: z
+    .string({
+      required_error: 'Color is required',
+      invalid_type_error: 'Color must be a string',
+    })
+    .min(3, { message: 'Color must be 3 or more characters long' }),
   status: z.boolean().optional(),
-  buyValue: z.number().int(),
+  buyValue: z
+    .number({
+      required_error: 'buyValue is required',
+      invalid_type_error: 'buyValue must be a number',
+    })
+    .int(),
 });
 
 export type IVehicle = z.infer<typeof vehicleZodSchema>;

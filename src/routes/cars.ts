3315456af07d7cd01288b6d2d581c9f744b13427
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-15 11:17:38 -0300

    feat(Routes): :sparkles: implementando o método read para a rota get /cars

--- src/routes/cars.ts
+++ src/routes/cars.ts
@@ -10,5 +10,6 @@ const carService = new CarService(carModel);
 const carController = new CarController(carService);
 
 route.post('/cars', (req, res) => carController.create(req, res));
+route.get('/cars', (req, res) => carController.read(req, res));
 
 export default route;
--- src/services/Car.ts
+++ src/services/Car.ts
@@ -15,6 +15,10 @@ class CarService implements IService<ICar> {
     }
     return this._car.create(parsed.data);
   }
+
+  public async read(): Promise<ICar[]> { 
+    return this._car.read();
+  }
 }
 
 export default CarService;

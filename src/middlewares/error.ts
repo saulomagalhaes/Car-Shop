08216f25ea2161ac702a3430872d65f69f01c519
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-15 11:59:32 -0300

    refactor(error): :recycle: refatorando para atender os lançamentos de erro solicitados

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -17,7 +17,7 @@ const errorHandler: ErrorRequestHandler = (
   const mappedError = errorCatalog[messageAsErrorType];
   if (mappedError) {
     const { httpStatus, message } = mappedError;
-    return res.status(httpStatus).json({ message });
+    return res.status(httpStatus).json({ error: message });
   }
   console.error(err);
   return res.status(500).json({ message: 'internal error' });

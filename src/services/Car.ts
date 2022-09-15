3946c6db53b16feff86dd18b5a059038ecb57f4d
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-15 12:01:05 -0300

    feat(CarService): :sparkles: implementando o método readOne

--- src/services/Car.ts
+++ src/services/Car.ts
@@ -1,3 +1,4 @@
+import { ErrorTypes } from '../errors/catalog';
 import { carZodSchema, ICar } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
 import { IService } from '../interfaces/IService';
@@ -18,6 +19,12 @@ class CarService implements IService<ICar> {
 
   public async read(): Promise<ICar[]> { 
     return this._car.read();
+  }
+
+  public async readOne(_id:string):Promise<ICar> {
+    const car = await this._car.readOne(_id);
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
   }
 }
 

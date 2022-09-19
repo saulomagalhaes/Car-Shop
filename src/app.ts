03af544de2b825d25ba5c4bacf97fe55d747ca5f
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-19 12:42:11 -0300

    feat(App): :sparkles: implementando CORS e swagger-ui-express para doc

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
 import errorHandler from './middlewares/error';
 import carRouter from './routes/cars';
 import motorcycleRouter from './routes/motorcycle';
+import swaggerDocument from './swagger.json';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(carRouter);
 app.use(motorcycleRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 

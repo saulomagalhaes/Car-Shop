84d306375fd817fd0bf65cfeeabe740e04358f0c
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-09-28 17:09:17 -0300

    refactor(App): :recycle: alterando o nome da rota de acesso a documentação

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(carRouter);
 app.use(motorcycleRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 
